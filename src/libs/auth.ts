eea2e8902e3735155a8604bc9138d9d4451154b6
Drilon <navycrack1@gmail.com>
2024-01-12 17:38:08 +0100

    created auth

---
+++ src/libs/auth.ts
@@ -0,0 +1,22 @@
+import  { NextAuthOptions } from 'next-auth';
+import { SanityAdapter, SanityCredentials } from 'next-auth-sanity';
+import GoogleProvider from 'next-auth/providers/google';
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+          }),
+          SanityCredentials(),
+    ],
+    session: {
+        strategy: 'jwt'
+      },
+      adapter: SanityAdapter(),
+      debug: process.env.NODE_ENV === 'development',
+      secret: process.env.NEXTAUTH_SECRET,
+      callbacks: {}
+}
+
