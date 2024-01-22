552379467276020de1be7152f617312dc99ea395
Drilon <navycrack1@gmail.com>
2024-01-23 00:09:10 +0100

    newsletter

--- src/components/NewsLetter/NewsLetter.tsx
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -9,16 +9,7 @@ const NewsLetter = () => {
             Sign Up for Our Newsletter
           </h6>
   
-          <div className='flex-col justify-center w-full md:flex-row flex pt-12'>
-            <input
-              type='email'
-              placeholder='Your email'
-              className='bg-[#026057] h-11 md:h-16 mb-2 md:mb-0 rounded-xl pl-6 md:mr-5 md:w-[452px] text-white placeholder:text-white focus:outline-none'
-            />
-            <button type='button' className='btn-tertiary'>
-              Subscribe
-            </button>
-          </div>
+       
         </form>
       </section>
     );
