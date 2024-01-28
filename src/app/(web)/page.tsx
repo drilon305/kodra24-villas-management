8288e68e03dfdede1e6dde81596d9a137ce92be1
Drilon <navycrack1@gmail.com>
2024-01-28 22:04:58 +0100

    building featuredroom section

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,13 +1,22 @@
+import FeaturedRoom from "@/components/FeaturedRoom/FeaturedRoom";
 import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import NewsLetter from "@/components/NewsLetter/NewsLetter";
+import { getFeaturedRoom } from "@/libs/apis";
 
-export default function Home() {
+const Home = async () => {
+
+  const featuredRoom = await getFeaturedRoom()
+
   return <>
   <HeroSection />
+  <FeaturedRoom featuredRoom={featuredRoom} />
   <Gallery />
   <NewsLetter />
   </>
     
   
 }
+
+
+export default Home;
