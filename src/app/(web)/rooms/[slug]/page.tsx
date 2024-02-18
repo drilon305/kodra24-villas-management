44a7266c9bdd03337b902e5368373c09fd7ee49c
Drilon <navycrack1@gmail.com>
2024-02-18 17:32:20 +0100

    building photos gallery and  icons

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -11,19 +11,47 @@ const RoomDetails = (props: { params: { slug: string } }) => {
   } = props;
 
   const fetchRoom = async () => getRoom(slug);
-  const {data: room, error, isLoading} = useSWR("/api/room", fetchRoom);
+  const { data: room, error, isLoading } = useSWR("/api/room", fetchRoom);
 
-  if (error) throw new Error('Cannot fetch data');
-  if (typeof room === 'undefined' && !isLoading)
-    throw new Error('Cannot fetch data');
+  if (error) throw new Error("Cannot fetch data");
+  if (typeof room === "undefined" && !isLoading)
+    throw new Error("Cannot fetch data");
 
-    if(!room) return <LoadingSpinner />
+  if (!room) return <LoadingSpinner />;
 
-    console.log(room)
+  console.log(room);
 
-  return <div>
-    <VillasGallery photos={room.images} />
-  </div>;
+  return (
+    <div>
+      <VillasGallery photos={room.images} />
+
+      <div className="container mx-auto mt-20">
+        <div className="md:grid md:grid-cols-12 gap-10 px-3">
+          <div className="md:col-span-8 md:w-full">
+            <div>
+              <h2 className="font-bold text-left text-lg md:text-2xl">
+                {room.name}
+              </h2>
+              <div className="flex my-11">
+                {room.offeredAmenities.map((ammenity) => (
+                  <div
+                    key={ammenity._key}
+                    className="md:w-44 w-fit text-center px-2 md:px-0 h-20 
+                  md:h-40 mr-3 bg-[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center"
+                  >
+                    <i className={`fa-solid ${ammenity.icon} md:text-2xl`}></i>
+                    <p className="text-xs md:text-base pt-3">{ammenity.amenity}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="md:col-span-4 rounded-xl shadow-lg dark:shadow dark:shadow-white sticky top-10 h-fit overflow-auto"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RoomDetails;
