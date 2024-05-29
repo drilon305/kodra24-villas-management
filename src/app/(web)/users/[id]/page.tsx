6f2a242a77ccc6e65340c8f5317f235ea722ac42
Drilon <navycrack1@gmail.com>
2024-05-29 23:37:29 +0200

    designing users page

--- src/app/(web)/users/[id]/page.tsx
+++ src/app/(web)/users/[id]/page.tsx
@@ -3,32 +3,40 @@
 import useSWR from "swr"
 import { getUserBookings } from "@/libs/apis"
 import axios from "axios";
+import { User } from "@/models/user";
 
 
 
-const UserDetails = (props: { params: { id: string }}) => {
-  const {params: { id: userId}} = props;
+const UserDetails = (props: { params: { id: string } }) => {
+  const {
+    params: { id: userId },
+  } = props;
 
-  const fetchUserBooking = async () => getUserBookings(userId)
+  const fetchUserBooking = async () => getUserBookings(userId);
   const fetchUserData = async () => {
-    const { data } = await axios.get('/api/users')
-    return data
-  }
+    const { data } = await axios.get<User>("/api/users");
+    return data;
+  };
 
-  const {data: userBookings, error, isLoading } = useSWR('/api/userbooking', )
+  const {
+    data: userBookings,
+    error,
+    isLoading,
+  } = useSWR("/api/userbooking", fetchUserBooking);
 
-  const {data: userData, isLoading: loadingUserData, error: errorGettingUserData} = useSWR('/api/users', fetchUserData)
+  const {
+    data: userData,
+    isLoading: loadingUserData,
+    error: errorGettingUserData,
+  } = useSWR("/api/users", fetchUserData);
 
-  if (error || errorGettingUserData) throw new Error('Cannot fetch data');
-  if (typeof userBookings === 'undefined' && !isLoading)
-    throw new Error('Cannot fetch data');
-
-  console.log(userBookings)
+  if (error || errorGettingUserData) throw new Error("Cannot fetch data");
+  if (typeof userBookings === "undefined" && !isLoading)
+    throw new Error("Cannot fetch data");
 
 
-  return (
-    <div>UserDetails</div>
-  )
-}
 
-export default UserDetails
+  return <div>UserDetails</div>;
+};
+
+export default UserDetails;
